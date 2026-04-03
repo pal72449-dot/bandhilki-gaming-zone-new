d3b1555911e61544c2eefd47a046e8e0d5cb771f
shivam <shivam@gmail.com>
2026-04-03 06:31:57 +0530

    added images to all games in games library

--- src/components/GamesLibrary.tsx
+++ src/components/GamesLibrary.tsx
@@ -1,11 +1,20 @@
+import Image from 'next/image';
 import styles from './GamesLibrary.module.css';
 
 export default function GamesLibrary() {
   const games = [
-    "FIFA 24", "GTA V", "WWE 2K24", "Cricket 24", 
-    "Call of Duty", "God of War", "Spider-Man", 
-    "Asphalt", "EA FC 25", "Mortal Kombat", 
-    "Dragon Ball", "Uncharted"
+    { name: "FIFA 24", image: "/images/games/fifa24.png" },
+    { name: "GTA V", image: "/images/games/gtav.png" },
+    { name: "WWE 2K24", image: "/images/games/wwe2k24.png" },
+    { name: "Cricket 24", image: "/images/games/cricket24.png" },
+    { name: "Call of Duty", image: "/images/games/cod.png" },
+    { name: "God of War", image: "/images/games/gow.png" },
+    { name: "Spider-Man", image: "/images/games/spiderman.png" },
+    { name: "Asphalt", image: "/images/games/asphalt.png" },
+    { name: "EA FC 25", image: "/images/games/eafc25.png" },
+    { name: "Mortal Kombat", image: "/images/games/mk.png" },
+    { name: "Dragon Ball", image: "/images/games/dbz.png" },
+    { name: "Uncharted", image: "/images/games/uncharted.png" }
   ];
 
   return (
@@ -18,7 +27,9 @@ export default function GamesLibrary() {
           {games.map((game, index) => (
             <div key={index} className={styles.gameCard}>
               <div className={styles.gameInner}>
-                <span className={styles.gameName}>{game}</span>
+                <Image src={game.image} alt={game.name} fill className={styles.gameImage} />
+                <div className={styles.gameOverlay}></div>
+                <span className={styles.gameName}>{game.name}</span>
               </div>
             </div>
           ))}
